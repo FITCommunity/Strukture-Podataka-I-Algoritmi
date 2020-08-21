3bb08da4cfb1c2f38a020060329f8f895cbcb596
Enis Mulić <enis.s.mulic@gmail.com>
2020-08-21 15:53:39 +0200

    Move helper functions to anonymous namespaces

--- ASP/ASP/QuickSort.h
+++ ASP/ASP/QuickSort.h
@@ -12,50 +12,50 @@ void QuickSort
 	QSort(array, 0, size, compare);
 }
 
-template<typename T>
-void QSort
-(
-	T* array,
-	int beginning,
-	int ending,
-	std::function<bool(const T&, const T&)> compare = [](const T& x, const T& y) {return x < y; }
-)
+namespace
 {
-	if (beginning < ending)
+	template<typename T>
+	void QSort
+	(
+		T* array,
+		int beginning,
+		int ending,
+		std::function<bool(const T&, const T&)> compare = [](const T& x, const T& y) {return x < y; }
+	)
 	{
-		int pivot = Partition(array, beginning, ending, compare);
-
-		QSort(array, beginning, pivot - 1);
-		QSort(array, pivot + 1, ending);
-	}
-}
-
-template<typename T>
-int Partition
-(
-	T* array,
-	int beginning,
-	int ending,
-	std::function<bool(const T&, const T&)> compare = [](const T& x, const T& y) {return x < y; }
-)
-{
-	T pivot = array[ending];
-	int i = beginning - 1;
-
-	for (int j = beginning; j < ending; j++)
-	{
-		if (compare(array[j], pivot))
+		if (beginning < ending)
 		{
-			i++;
-			std::swap(array[i], array[j]);
+			int pivot = Partition(array, beginning, ending, compare);
+
+			QSort(array, beginning, pivot - 1);
+			QSort(array, pivot + 1, ending);
 		}
 	}
 
-	i++;
-	std::swap(array[i], array[ending]);
+	template<typename T>
+	int Partition
+	(
+		T* array,
+		int beginning,
+		int ending,
+		std::function<bool(const T&, const T&)> compare = [](const T& x, const T& y) {return x < y; }
+	)
+	{
+		T pivot = array[ending];
+		int i = beginning - 1;
 
-	return i;
+		for (int j = beginning; j < ending; j++)
+		{
+			if (compare(array[j], pivot))
+			{
+				i++;
+				std::swap(array[i], array[j]);
+			}
+		}
+
+		i++;
+		std::swap(array[i], array[ending]);
+
+		return i;
+	}
 }
-
-
-
